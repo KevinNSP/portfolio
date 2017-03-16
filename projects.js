e5f66b5866eeae421f81fa80290a772cce4c304a
Kevin Schmidt <NSPtuning@gmail.com>
2017-03-16 12:40:14 -0700

    still working on getting the information to the DOM

--- projects.js
+++ projects.js
@@ -3,9 +3,22 @@
 var rawData = [
   {
     title: 'project 1',
-    category: 'project 1 category',
-    background: 'project one image',
-    projectUrl: 'project URL',
+    details: 'project 1 details',
+    background: 'project 1 image',
+    projectUrl: 'project 1 URL',
+  },
 
+  {
+    title: 'project 2',
+    details: 'project 2 details',
+    background: 'project 2 image',
+    projectUrl: 'project 2 URL',
+  },
+  
+  {
+    title: 'project 3',
+    details: 'project 3 details',
+    background: 'project 3 image',
+    projectUrl: 'project 3 URL',
   }
 ];
