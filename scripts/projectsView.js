b6bd9cc235c1e1cf4638c793f482ae3602265632
Kevin Schmidt <NSPtuning@gmail.com>
2017-03-17 11:50:13 -0700

    switches to about me and thats about it

--- scripts/projectsView.js
+++ scripts/projectsView.js
@@ -1,22 +1,28 @@
 'use strict';
 
-var projectsView = {};
+var projectsView = [];
 
-projectsView.populateFilters = function() {
+// projectsView.populateFilters = function() {
+//   $('article').not('.template').each(function() {
+//     var authorName, category, optionTag;
+//     authorName = $(this).find('address a').text();
+//     optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+//     $('#author-filter').append(optionTag);
+//     category = $(this).attr('data-category');
+//     optionTag = '<option value="' + category + '">' + category + '</option>';
+//     if ($('#category-filter option[value="' + category + '"]').length === 0) {
+//       $('#category-filter').append(optionTag);
+//     }
+//   });
+// };
 
+projectsView.handleMainNav = function () {
+  $('.navbar').on('click', 'a[href="#about"]', function() {
+    console.log(this);
+    $('.projectsContent, .aboutMeContent').hide();
+    $('.aboutMeContent').fadeIn('slow');
+  });
+  $('.main-nav .tab:first').click();
+};
 
-
-
-
-
-
-
-  $('article').not('.template').each(function() {
-    var authorName, category, optionTag;
-    authorName = $(this).find('address a').text();
-    optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
-    $('#author-filter').append(optionTag);
-    category = $(this).attr('data-category');
-    optionTag = '<option value="' + category + '">' + category + '</option>';
-    if ($('#category-filter option[value="' + category + '"]').length === 0) {
-      $('#category-filter').append(optionTag);
+projectsView.handleMainNav();
