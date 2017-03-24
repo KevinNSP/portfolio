9d1fd6cb033600ffd9e19ed4c3f05171e8783f58
Kevin Schmidt <NSPtuning@gmail.com>
2017-03-24 10:06:23 -0700

    moved files to public folder

---
+++ public/scripts/project.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+function Projects (projectOptions) {
+  this.title = projectOptions.title;
+  this.details = projectOptions.details;
+  this.picture = projectOptions.picture;
+  this.projectUrl = projectOptions.projectUrl;
+}
+
+const projects = [];
+
+Projects.prototype.toHtml = function() {
+  var templateRender = Handlebars.compile($('#projectsTemplate').text());
+  return templateRender(this);
+};
+
+$.getJSON('/data/projectsData.json', function(response) {
+  response.forEach(function(projectObject) {
+    projects.push(new Projects(projectObject))
+  });
+  projects.forEach(function(project) {
+    $('#projects').append(project.toHtml());
+  })
+})
